2fdc3a63592d284bc952bb2c700be9a7685a4dc6
Blientz <BCL39@pitt.edu>
2025-06-05 19:27:07 -0400

    Initial firebase intigration

---
+++ src/js/firebase.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const admin = require('firebase-admin');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+const auth = admin.auth();
+
+module.exports = { admin, db, auth };

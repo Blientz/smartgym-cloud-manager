89f7fc3e513c161c300f343f4a45d042feca99ff
Blientz <BCL39@pitt.edu>
2025-06-05 14:10:08 -0400

    Clean URL for Web App

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'admin.html'));
+});
+
+app.get('/qr', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'qr.html'));
+});
+
+app.get('/userLogin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'userLogin.html'));
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
